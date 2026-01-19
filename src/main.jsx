1bcec23249bc8a192bb4e850402bb3cd5fa28b86
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-01-18 23:46:53 -0300

    feat: switch to hashRouter for gh-pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import TaskPage from './pages/taskPage.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
